57558ad247409668616dbaf0f06a37f7eba6d944
lj5619 <leon.olaprath@gmail.com>
2025-05-22 16:37:48 +0530

    [Leon] Add: Configure and db files

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from db.database import engine, Base
+from user import route as user_route
+from notes import route as notes_route
+from label.route import label_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(user_route.router)
+app.include_router(notes_route.router)
+app.include_router(label_router)
